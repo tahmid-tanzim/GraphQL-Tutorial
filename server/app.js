db367fb9c6e9375d2b6a1f5c7973d98cbd259e01
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2019-10-22 16:58:30 +0600

    Setting up GraphQL

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const GraphQL_HTTP = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', GraphQL_HTTP({
+
+}));
 
 app.listen(4000, () => {
     console.log('App listening on PORT 4000');
