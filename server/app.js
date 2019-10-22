e5f1f7fd8be81e9c3f03a8ba32dfd361ed87ae13
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2019-10-22 21:29:46 +0600

    GraphQL - The Resolve Function

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const GraphQL_HTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', GraphQL_HTTP({
-
+    schema
 }));
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,27 +1,32 @@
-const graphql = require('graphql');
-
 const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLSchema
-} = graphql;
+} = require('graphql');
+
+// Dummy Data
+const books = [
+    {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+    {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString}
     })
 });
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        book: { type: BookType },
-        args: { id: { type: GraphQLString }},
+        book: {type: BookType},
+        args: {id: {type: GraphQLString}},
         resolve: (parent, args) => {
-
+            return books.find(({id}) => args.id === id)
         }
     }
 });
